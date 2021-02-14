a341a939af862b3341792a54937aa25b18a190bc
nopallllll <79029289+nopallllll@users.noreply.github.com>
2021-02-14 15:21:51 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,134 @@
+const help = (prefix) => { 
+	return `
+╭▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂╮
+┃╭┅┈⪼ *EREN YEAGER BOT─╝*
+┊╰⧿─╗ *THANX TO ALLAH*
+┊ ⛬ ╔╯ *VERSION : V4.104.020*
+┠⧿⧿╯⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿⧿
+*┃⛨⧿✧ུNama : Eren Yeager BOT*
+*┃⛨⧿✧ུFounder : Arifi Razzaq*
+*┃⛨⧿✧ུStatus : ACTIVE*
+*┃⛨⧿✧ུPrefix : ❪ ${prefix} ❫*
+╰⧿╗
+┎⧿┸⧿⪼⦁ *UNTUK INDONESIA*
+┠➤ *Total : 382 Project*
+┠➤ *Update : Rabu 10-02-2021*
+┠➤ *Thanx To : Arifi Razzaq*
+╰▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+╭━━━━━━━━━⛚⛚⛚⛚⛚⛚
+┠ *MENU ALL PROJECT*
+╰━━⛚⛚⛚⛚⛚⛚⛚━━━━━━╯
+
+⧼⟞ *${prefix}ownermenu*
+⧼⟞ *${prefix}adminmenu*
+⧼⟞ *${prefix}funmenu*
+⧼⟞ *${prefix}mediamenu*
+⧼⟞ *${prefix}kerangmenu*
+⧼⟞ *${prefix}makermenu*
+⧼⟞ *${prefix}othermenu*
+⧼⟞ *${prefix}animemenu*
+⧼⟞ *${prefix}nsfwmenu*
+⧼⟞ *${prefix}vipmenu*
+⧼⟞ *${prefix}wibumenu*
+⧼⟞ *${prefix}hackingmenu* 🆕
+
+╭━━━━━━━━━⛚⛚⛚⛚⛚⛚
+┠ *GENERAL SCOPE*
+╰━━⛚⛚⛚⛚⛚⛚⛚━━━━━━╯
+
+⧼⟞ *${prefix}bugreport*
+⧼⟞ *${prefix}info*
+⧼⟞ *${prefix}owner*
+⧼⟞ *${prefix}request*
+⧼⟞ *${prefix}setprefix*
+⧼⟞ *${prefix}listblock*
+⧼⟞ *${prefix}iklan*
+⧼⟞ *${prefix}runtume*
+⧼⟞ *${prefix}rules*
+⧼⟞ *${prefix}tnc*
+⧼⟞ *${prefix}laporbug*
+⧼⟞ *${prefix}cekvip*
+⧼⟞ *${prefix}daftarvip*
+⧼⟞ *${prefix}addvip*
+⧼⟞ *${prefix}dellvip*
+⧼⟞ *${prefix}snk*
+⧼⟞ *${prefix}listpremium*
+⧼⟞ *${prefix}donate*
+⧼⟞ *${prefix}fitnah*
+⧼⟞ *${prefix}totaluser*
+⧼⟞ *${prefix}level*
+⧼⟞ *${prefix}glass*
+⧼⟞ *${prefix}cekup* 🆕
+⧼⟞ *${prefix}afk* 🆕
+
+╭━━━━━━━━━⛚⛚⛚⛚⛚⛚
+┠ *FITUR POPULER*
+╰━━⛚⛚⛚⛚⛚⛚⛚━━━━━━╯
+
+⧼⟞ *${prefix}apiteks [text]*
+⧼⟞ *${prefix}tahta [text]*
+⧼⟞ *${prefix}airteks [text]*
+⧼⟞ *${prefix}metalteks [text]*
+⧼⟞ *${prefix}mlogo [text]*
+⧼⟞ *${prefix}ffbaner [text]*
+⧼⟞ *${prefix}embun [text]*
+⧼⟞ *${prefix}kunciteks [text]*
+⧼⟞ *${prefix}say*
+⧼⟞ *${prefix}addsay [text]*
+⧼⟞ *${prefix}listsay*
+⧼⟞ *${prefix}bacot*
+⧼⟞ *${prefix}addbacot [text]*
+⧼⟞ *${prefix}listbacot*
+⧼⟞ *${prefix}resetbacot*
+⧼⟞ *${prefix}aguse*
+⧼⟞ *${prefix}kicktime*
+⧼⟞ *${prefix}nobg*
+⧼⟞ *${prefix}url2img*
+⧼⟞ *${prefix}joox*
+⧼⟞ *${prefix}asupan*
+⧼⟞ *${prefix}virtex* 🆕
+⧼⟞ *${prefix}cogans* 🆕
+⧼⟞ *${prefix}cecans* 🆕
+⧼⟞ *${prefix}wame* 🆕
+⧼⟞ *${prefix}animehug* 🆕
+⧼⟞ *${prefix}spamcall [8xxxx]* 🆕
+┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+⚘ *MODE PENGEMBANG:*
+
+⪼⦁ *${prefix}leveling [Enable/Disable]*
+⪼⦁ *${prefix}Anime [1/0]*
+⪼⦁ *${prefix}nsfw [1/0]*
+⪼⦁ *${prefix}welcome [1/0]*
+⪼⦁ *${prefix}publik* 🆕
+⪼⦁ *${prefix}killerbee* 🆕
+⪼⦁ *${prefix}antitrace* 🆕
+⪼⦁ *${prefix}antilink [1/0]* 🆕
+⪼⦁ *${prefix}antivirus* 🆕
+⪼⦁ *${prefix}antiban* 🆕
+⪼⦁ *${prefix}antispam* 🆕
+┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+⚘ *AKSESBILITAS :*
+
+⪼⦁ *${prefix}lampuinfo* 🆕
+⪼⦁ *${prefix}datanegara* 🆕
+┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+⚘ *EREN YEAGER BUSSINESS :*
+
+⪼⦁ *${prefix}kontakdeveloper*
+⪼⦁ *${prefix}daftarmemberselebgram*
+┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+◪ *SPECIAL BIG THANKS TO :*
+
+*© MhankBar2 | Arugia | Adiwajshing | xptn | tobz | Pengguna bot | maslent | VHTears | Nazwa | Arifi Razzaq | Agusecaliph | Rey | rizki | Aulia | natasya | Drawl Nag | Nayla | Khadijah | Aurel | Galuh | Ramlan | Narutomo | Anker | All. Creator Bot lainnya.*
+┅─┅─┅─┅─┅─┅─┅─┅─┅─┅─ ❁۪۪
+
+          ▉║█▐▉▉▐▐▍█║▍▉▏▍▍
+          ▉║█▐▉▉▐▐▍█║▍▉▏▍▍
+   
+             *© Arifi Razzaq*
+             
+*Jangan Lupa Donasi ”_“*
+https://saweria.co/Arifirazzaq2001
+`
+}
+exports.help = help
